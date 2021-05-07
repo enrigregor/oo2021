6c658d9e98bf1a2dc0270c0d57b3686c6536beb9
enrigregor <bigirne@tlu.ee>
2021-05-07 13:52:12 +0300

    KT kolmas commit

--- w13_kontrolltoo2/Main.java
+++ w13_kontrolltoo2/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         
+        Word word = new Word("testiks", 't');
+
+        System.out.println(word.getLetterCount());
+
     }
 }
--- w13_kontrolltoo2/Word.java
+++ w13_kontrolltoo2/Word.java
@@ -1,14 +1,22 @@
 public class Word implements LetterInterface{
     private String word;
+    private char letter;
 
-    public Word(String word){
+    public Word(String word, char letter){
         this.word = word;
+        this.letter = letter;
     }
 
     @Override
     public int getLetterCount() {
-        
-        return 0;
+        int length = word.length() -1;
+        int counter = 0;
+        for (int i = 0; length > i; i++){
+            if (word.charAt(i) == letter){
+                counter++;
+            }
+        }
+        return counter;
     }
     
 }
