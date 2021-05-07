34a40e3767e08c7e55607ec8f25a60328400b5c2
enrigregor <bigirne@tlu.ee>
2021-05-07 13:31:08 +0300

    Kontrolltöö teine commit

---
+++ w13_kontrolltoo2/LetterInterface.java
@@ -0,0 +1,3 @@
+public interface LetterInterface {
+    public int getLetterCount();
+}
---
+++ w13_kontrolltoo2/Word.java
@@ -0,0 +1,14 @@
+public class Word implements LetterInterface{
+    private String word;
+
+    public Word(String word){
+        this.word = word;
+    }
+
+    @Override
+    public int getLetterCount() {
+        
+        return 0;
+    }
+    
+}
